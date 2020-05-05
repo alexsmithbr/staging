de44976580b9460136716f4f806f46921695f1a9
Alexandre Heitor Schmidt <alexandre.schmidt@gmail.com>
2020-05-05 20:50:42 -0300

    At this point, breakfast max1 works.

--- device.mk
+++ device.mk
@@ -19,7 +19,7 @@ $(call inherit-product-if-exists, $(QCPATH)/common/config/device-vendor.mk)
 endif
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 PRODUCT_DEX_PREOPT_MODULE_CONFIGS := true
--- lineage_max1.mk
+++ lineage_max1.mk
@@ -1,5 +1,5 @@
-#
-# Copyright 2020 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,17 +12,28 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l_mr1.mk)
 
 # Inherit from max1 device
 $(call inherit-product, device/letv/max1/device.mk)
 
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
 PRODUCT_NAME := lineage_max1
 PRODUCT_DEVICE := max1
 PRODUCT_MANUFACTURER := Letv
-PRODUCT_MODEL := max1
 PRODUCT_BRAND := Letv
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="Max1-user 6.0.1 MMB29M 7 dev-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-letv
+
+BUILD_FINGERPRINT := "Letv/Max1/Max1:6.0.1/MMB29M/1447858500:user/release-keys"
+
+TARGET_VENDOR := letv
